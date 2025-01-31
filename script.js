68dcecac05df036f795b232ba785a6da8c899e37
ishayashika <122111684+ishayashika@users.noreply.github.com>
2025-02-01 00:01:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function makeid(l) {
   // write your code here
+	let str="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+	let ans="";
+	for(let i=0;i<l;i++){
+		let randomIndex=Math.floor(Math.random()*str.length);
+		ans+=str[randomIndex];
+	}
+	return ans;
 }
 
 // Do not change the code below.
